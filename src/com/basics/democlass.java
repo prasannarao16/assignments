917085ade3ace6f106ab5e3257bd565cdfad2721
prasannarao16 <prasannapinninti12@gmail.com>
2022-11-18 23:10:36 -0500

    first commit

---
+++ src/com/basics/democlass.java
@@ -0,0 +1,8 @@
+package com.basics;
+
+public class democlass {
+    public static void main(String[] args){
+        System.out.println("welcome to the world");
+    }
+
+}
---
+++ src/com/basics/helloworld.java
@@ -0,0 +1,19 @@
+package com.basics;
+
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("           **    **      **       **  **            **");
+        System.out.println("           **    **      **        **   **          **");
+        System.out.println("           **     **     **        **     **        **");
+        System.out.println("           **    **      **        **        **      **");
+        System.out.println("           **  **        **        **        **      **");
+        System.out.println("           **            **        **          **    **");
+        System.out.println("           **            **        **           **   **");
+        System.out.println("           **            **        **            **  **");
+        System.out.println("           **            **        **                **");
+        System.out.println();
+    }
+
+}
